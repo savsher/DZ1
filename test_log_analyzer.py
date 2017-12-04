2a8bbb46505f29857e99ef784663e21c6e4b1d83
MrX <savsher@gmail.com>
2017-12-05 00:07:44 +0300

    Tue Dec  5 00:07:44 MSK 2017

--- test_log_analyzer.py
+++ test_log_analyzer.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import log_analyzer
-
 import unittest
+import random
+import log_analyzer
+import os
 
 class TestLogAnalyzer(unittest.TestCase):
-
     def setUp(self):
+        self.tmp_conf_file = '/var/tmp/test'+ str(random.randint(1,999))
+        self.tmp_conf_file_str = "[GLOBAL]\nREPORT_SIZE: 333\nREPORT_DIR: ./reports\n" \
+                                 "LOG_DIR: ./log\nTS_FILE: /var/tmp/log_analyzer.ts"
+        with open(self.tmp_conf_file,'wt') as f:
+            f.write(self.tmp_conf_file_str)
+
+    def test_read_config(self):
+        log_analyzer.read_config(self.tmp_conf_file)
+        self.assertEqual(333, log_analyzer.config["REPORT_SIZE"])
+        self.assertEqual('./log', log_analyzer.config["LOG_DIR"])
+        self.assertEqual('./reports', log_analyzer.config["REPORT_DIR"])
+
+    def test_kkk(self):
         pass
 
     def tearDown(self):
-        pass
+        os.remove(self.tmp_conf_file)
 
-    def test(self):
-        a= 'a'
-        b= 'a'
-        self.assertEqual(a,b)
+
+
