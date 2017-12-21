fa63b199a886598288cb3238d10cba25a0b2626a
MrX <savsher@gmail.com>
2017-12-21 14:20:08 +0300

    Thu Dec 21 14:20:08 MSK 2017

--- log_analyzer.py
+++ log_analyzer.py
@@ -73,6 +73,21 @@ def time_stamp(path):
         logging.exception(err)
         sys.exit(1)
 
+def check_report_file(conf, http_log_time):
+    """
+    Check the report file existence
+    if not return file name
+    """
+    report_file = os.path.join(
+        conf['REPORT_DIR'],
+        'report-' + time.strftime('%Y.%m.%d',time.strptime(str(http_log_time),'%Y%m%d')) + '.html')
+    if os.path.isfile(report_file):
+        logging.info('Report already exists : %s ', report_file)
+        time_stamp(conf['TS_FILE'])
+        logging.info('Script stop %s', sys.argv[0])
+        sys.exit(0)
+    return report_file
+
 
 def check_run(conf):
     """
@@ -100,17 +115,10 @@ def check_run(conf):
             if http_log_time < int(x[0]):
                 http_log_time = int(x[0])
                 http_log_file = name
-        break # 1 - depth
+        break # depth tree 1
     if http_log_file is not None:
         http_log_file = os.path.join(conf['LOG_DIR'], http_log_file)
-        report_file = os.path.join(conf['REPORT_DIR'],
-            'report-' + time.strftime('%Y.%m.%d', time.strptime(str(http_log_time), '%Y%m%d')) + '.html')
-        # check
-        if os.path.isfile(report_file):
-            logging.info('Report already exists : %s ', report_file)
-            time_stamp(conf['TS_FILE'])
-            logging.info('Script stop %s', sys.argv[0])
-            sys.exit(0)
+        report_file = check_report_file(conf, http_log_time)
         return {'log': http_log_file, 'report': report_file}
     else:
         logging.info('No log file to grep ...')
@@ -125,7 +133,11 @@ def grep_file(filed, trigger):
     :return dic()
     """
     # dict( url:[count, time_sum, time_min, time_max])
-    grep_data = defaultdict(list)
+    request_data = defaultdict(list)
+    URL_COUNT = 0
+    URL_TIME_SUM = 1
+    URL_TIME_MIN = 2
+    URL_TIME_MAX = 3
     # save file-modification in str format
     search_tmpl = re.compile('[GET|POST] .* HTTP')
     pattern_lines = 0
@@ -137,53 +149,57 @@ def grep_file(filed, trigger):
                 pattern_lines += 1
                 rtime = float(line.split()[-1])
                 request = request.group().split()[1]
-                if request in grep_data.keys():
-                    grep_data[request][0] += 1
-                    grep_data[request][1] += rtime
-                    if grep_data[request][2] > rtime:
-                        grep_data[request][2] = rtime
-                    if grep_data[request][3] < rtime:
-                        grep_data[request][3] = rtime
+                if request in request_data.keys():
+                    request_data[request][URL_COUNT] += 1
+                    request_data[request][URL_TIME_SUM] += rtime
+                    if request_data[request][URL_TIME_MIN] > rtime:
+                        request_data[request][URL_TIME_MIN] = rtime
+                    if request_data[request][URL_TIME_MAX] < rtime:
+                        request_data[request][URL_TIME_MAX] = rtime
                 else:
-                    grep_data[request].append(1)
-                    grep_data[request].append(rtime)
-                    grep_data[request].append(rtime)
-                    grep_data[request].append(rtime)
+                    request_data[request].append(1)
+                    request_data[request].append(rtime)
+                    request_data[request].append(rtime)
+                    request_data[request].append(rtime)
             if float(total_lines + 1 - pattern_lines)/(total_lines + 1) > trigger:
                 logging.exception('Exceeded trashhold "BAD LINKS" : %s', trigger)
-                grep_data.clear()
+                request_data.clear()
                 break
     except IOError as err:
         logging.error(err)
         sys.exit(1)
     else:
-        return grep_data
+        return request_data
 
 
-def create_report(grep_data, reportf, conf):
+def create_report(request_data, reportf, conf):
     """
     Fun generate report file : report-YYYY.MM.DD.html
     grep_data :  dir()
     reportf : str(report filename
     conf: config)
      """
+    URL_COUNT = 0
+    URL_TIME_SUM = 1
+    URL_TIME_MIN = 2
+    URL_TIME_MAX = 3
     precise = 7
     result_data = list()
     total_req = 0
     total_time = 0
-    for x in grep_data:
-        total_req += grep_data[x][0]
-        total_time += grep_data[x][1]
+    for x in request_data:
+        total_req += request_data[x][URL_COUNT]
+        total_time += request_data[x][URL_TIME_SUM]
     result_data = list()
-    for k, v in sorted(grep_data.items(), key=lambda x: x[1][1], reverse=True)[0:int(conf['REPORT_SIZE'])]:
+    for k, v in sorted(request_data.items(), key=lambda x: x[1][URL_TIME_SUM], reverse=True)[0:int(conf['REPORT_SIZE'])]:
         tmpdir = {'url': k}
-        tmpdir['count'] = v[0]
-        tmpdir['count_perc'] = round(float(v[0]) / total_req, precise)
-        tmpdir['time_avg'] = round(float(v[1]) / v[0], precise)
-        tmpdir['time_max'] = round(v[3], precise)
-        tmpdir['time_med'] = round(float(v[3] - v[2]) / 2, precise)
-        tmpdir['time_perc'] = round(float(v[1]) / total_time, precise)
-        tmpdir['time_sum'] = round(v[1], precise)
+        tmpdir['count'] = v[URL_COUNT]
+        tmpdir['count_perc'] = round(float(v[URL_COUNT]) / total_req, precise)
+        tmpdir['time_avg'] = round(float(v[URL_TIME_SUM]) / v[URL_COUNT], precise)
+        tmpdir['time_max'] = round(v[URL_TIME_MAX], precise)
+        tmpdir['time_med'] = round(float(v[URL_TIME_MAX] - v[URL_TIME_MIN]) / 2, precise)
+        tmpdir['time_perc'] = round(float(v[URL_TIME_SUM]) / total_time, precise)
+        tmpdir['time_sum'] = round(v[URL_TIME_SUM], precise)
         result_data.append(tmpdir)
     # Rendering template
     try:
